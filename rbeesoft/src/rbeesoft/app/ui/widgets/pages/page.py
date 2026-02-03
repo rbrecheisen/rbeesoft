05dd6b4b5f7e52cb7dec2376c074bb48a137851d
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2026-02-03 12:42:00 +0100

    Deployed version 2.0.10 to PyPI

--- rbeesoft/src/rbeesoft/app/ui/widgets/pages/page.py
+++ rbeesoft/src/rbeesoft/app/ui/widgets/pages/page.py
@@ -5,12 +5,11 @@ from PySide6.QtWidgets import QWidget
 class Page(QWidget):
     page_changed = Signal(str)
 
-    def __init__(self, name, title, settings, license=None):
+    def __init__(self, name, title, settings):
         super(Page, self).__init__()
         self._name = name
         self._title = title
         self._settings = settings
-        self._license = license
 
     def name(self):
         return self._name
@@ -20,9 +19,6 @@ class Page(QWidget):
     
     def settings(self):
         return self._settings
-    
-    def license(self):
-        return self._license
     
     def switch_to_page(self, name):
         self.page_changed.emit(name)
